28789ef2360e98e22eb991fa1b65905a7813e9ba
lukasSurna <lukasurna@gmail.com>
2023-09-20 14:32:12 +0300

    funkcijos pass

--- grybai.py
+++ grybai.py
@@ -18,15 +18,33 @@ class Grybai(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     pavadinimas = Column(String(30))
     grybo_klase = Column(String(30))
+    vietove_id = Column(Integer, ForeignKey('vietove_id'))
+    vietoves = relationship('Vietoves', back_populates='grybai')
 
 class Vietoves(Base):
     __tablename__ = 'vietoves'
     id = Column(Integer, primary_key=True, autoincrement=True)
     vietoves_pavadinimas = Column(String(50))
+    grybai = relationship('Grybai', back_populates='vietoves')
 
 class Regionai(Base):
     __tablename__ = 'regionai'
     id = Column(Integer, primary_key=True, autoincrement=True)
     regionas = Column(String(50))
+    vietove_reg = Column(Integer, ForeignKey='vietoves.id')
+    vietove = relationship('Vietoves', back_populates='regionai')
 
 Base.metadata.create_all(db_engine)
+
+
+def main_window():
+    pass
+
+def add_region():
+    pass
+
+def add_location():
+    pass
+
+def add_mushroom():
+    pass
